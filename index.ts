2876810bd1b30f12cc0fab2e54590f6c1aabcd0c
waptik <687551+waptik@users.noreply.github.com>
2022-08-27 11:01:53 +0000

    initial commit

---
+++ index.ts
@@ -0,0 +1,15 @@
+import {writeFile} from "fs/promises";
+import epub from "epub-gen-memory";
+import { contentAlice, optionsAlice } from "./aliceData";
+
+(async () => {
+  const content = await epub(optionsAlice, contentAlice);
+  await writeFile(`${__filename.slice(0, -3)}.epub`, Buffer.from(content));
+
+  optionsAlice.numberChaptersInTOC = false;
+  const content2 = await epub(optionsAlice, contentAlice);
+  await writeFile(
+    `${__filename.slice(0, -3)}_nonum.epub`,
+    Buffer.from(content2),
+  );
+})();
